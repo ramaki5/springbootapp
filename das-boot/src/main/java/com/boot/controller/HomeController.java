614bc2c965f8b6401f25598a9b498967d020a371
ramaki5 <rk.mundru@gmail.com>
2018-09-30 00:53:29 +0530

    First Commit

---
+++ das-boot/src/main/java/com/boot/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home()
+	{
+		return "Das Boot app staretd successfully...!";
+	}
+}
